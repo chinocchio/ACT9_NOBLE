f3a4c7ed9a5cddde03af35139c036e076d0667cf
chinocchio <77035738+chinocchio@users.noreply.github.com>
2024-05-22 21:01:57 -0800

    act9 done

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
+
+
+Route::resource('products', ProductController::class);
+   
+
